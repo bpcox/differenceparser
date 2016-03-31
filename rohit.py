0e967df060d7bcb2ea8fea92c91da5981fdc67ab
NativeForeigner <nativeforeignerwiki@gmail.com>
2016-03-30 20:42:45 -0700

    commit code

---
+++ rohit.py
@@ -0,0 +1,16 @@
+import os
+
+
+#loop through
+for i in os.listdir(os.getcwd()):
+    if i.endswith(".log"):
+        try:
+            file = open(i)
+            lines = file.readlines()
+            total = int(lines[0].split(" ",1)[0])
+            duplicate = int(lines[3].split(" ",1)[0])
+            result = total-duplicate
+            print "Result for {}: {}\n".format(i, result)
+        except:
+            print "Invalid File {}\n".format(i)
+
